8cf628fd68cb489930e169e65cc67ed51d6bb8b7
RajendraKumarV <rajendravaradaraju@example.com>
2025-04-20 20:29:06 +0530

    Learnt rops

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import ListGroup from './components/ListGroup';
 
 function App() {
+    let items = ["Anchipura", "Itamadu", "Mysuru", "Bangalore"];
+
+    const handleSelectItem =(item:string)=>{
+         console.log(item)
+    }
   return (
     <div>
-      <ListGroup/>
+      <ListGroup items={items} heading='Cities' onSelectItem={handleSelectItem} />
     </div>
   );
 }
